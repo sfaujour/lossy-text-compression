67c81f67e2e1e5e4b7eb63f4758baea1665dbcd6
Sebastian Faujour <sebastian@picdrop.de>
2020-07-17 17:29:37 +0200

    Add chat

--- go.mod
+++ go.mod
@@ -1,3 +1,16 @@
 module github.com/sfaujour/lossy-text-compression
 
 go 1.14
+
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/gorilla/websocket v1.4.2
+	github.com/labstack/echo v3.3.10+incompatible
+	github.com/labstack/gommon v0.3.0 // indirect
+	github.com/mattn/go-colorable v0.1.7 // indirect
+	github.com/valyala/fasttemplate v1.2.0 // indirect
+	golang.org/x/crypto v0.0.0-20200709230013-948cd5f35899 // indirect
+	golang.org/x/net v0.0.0-20200707034311-ab3426394381 // indirect
+	golang.org/x/sys v0.0.0-20200625212154-ddb9806d33ae // indirect
+	golang.org/x/text v0.3.3 // indirect
+)
--- main.go
+++ main.go
@@ -3,7 +3,11 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"github.com/gorilla/websocket"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 	"html/template"
+	"io"
 	"log"
 	"net/http"
 	"os"
@@ -11,6 +15,31 @@ import (
 	"strings"
 	"syscall"
 )
+
+type Template struct {
+	templates *template.Template
+}
+
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
+var clients = make(map[*websocket.Conn]bool) // connected clients
+var broadcast = make(chan Message)           // broadcast channel
+
+// Configure the upgrader
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+// Define our message object
+type Message struct {
+	Email    string `json:"email"`
+	Username string `json:"username"`
+	Message  string `json:"message"`
+}
 
 func main() {
 	signalChannel := make(chan os.Signal, 1)
@@ -26,23 +55,86 @@ func main() {
 		}
 	}()
 
-	tmpl := template.Must(template.ParseFiles("index.html"))
+	t := &Template{
+		templates: template.Must(template.ParseGlob("public/index.html")),
+	}
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != http.MethodPost {
-			_ = tmpl.Execute(w, nil)
-			return
+	e := echo.New()
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	e.Pre(middleware.RemoveTrailingSlash())
+	e.Use(middleware.BodyDump(func(c echo.Context, reqBody, resBody []byte) {}))
+	e.Renderer = t
+	e.Match([]string{"GET", "POST"}, "/", Home)
+
+	e.Static("/chat", "public/chat")
+	// Create a simple file server
+
+	// Configure websocket route
+	e.GET("/ws", handleConnections)
+
+	// Start listening for incoming chat messages
+	go handleMessages()
+
+	e.Logger.Fatal(e.Start(":1234"))
+}
+
+func Home(c echo.Context) error {
+	if c.Request().Method != http.MethodPost {
+		return c.Render(http.StatusOK, "index.html", nil)
+	}
+
+	inputText := c.FormValue("inputText")
+	fmt.Println(inputText)
+	result := ltcWalker(inputText)
+	fmt.Println(result)
+
+	return c.Render(http.StatusOK, "index.html", struct{ Result string }{result})
+}
+
+func handleConnections(c echo.Context) error {
+	// Upgrade initial GET request to a websocket
+	ws, err := upgrader.Upgrade(c.Response(), c.Request(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Make sure we close the connection when the function returns
+	defer ws.Close()
+
+	// Register our new client
+	clients[ws] = true
+
+	for {
+		var msg Message
+		// Read in a new message as JSON and map it to a Message object
+		err := ws.ReadJSON(&msg)
+		if err != nil {
+			c.Logger().Errorf("error: %v", err)
+			delete(clients, ws)
+			break
 		}
+		// Send the newly received message to the broadcast channel
+		msg.Message = ltcWalker(msg.Message)
+		broadcast <- msg
+	}
 
-		inputText := r.FormValue("inputText")
-		fmt.Println(inputText)
-		result := ltcWalker(inputText)
-		fmt.Println(result)
+	return nil
+}
 
-		tmpl.Execute(w, struct{ Result string }{result})
-	})
-
-	log.Fatal(http.ListenAndServe(":1234", nil))
+func handleMessages() {
+	for {
+		// Grab the next message from the broadcast channel
+		msg := <-broadcast
+		// Send it out to every client that is currently connected
+		for client := range clients {
+			err := client.WriteJSON(msg)
+			if err != nil {
+				log.Printf("error: %v", err)
+				client.Close()
+				delete(clients, client)
+			}
+		}
+	}
 }
 
 func ltcWalker(input string) string {
